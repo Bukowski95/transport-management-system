2da2b9842536704464da427d6c57af36806f160f
bukowski <strawdog937@gmail.com>
2025-12-10 15:39:51 +0530

    Add single-parameter constructor to InvalidStatusTransitionException
    Add constructor that accepts a single message string to support
    existing usage patterns in service classes.

--- src/main/java/com/koustav/tms/exception/InvalidStatusTransitionException.java
+++ src/main/java/com/koustav/tms/exception/InvalidStatusTransitionException.java
@@ -2,11 +2,15 @@ package com.koustav.tms.exception;
 
 
 public class InvalidStatusTransitionException extends RuntimeException {
-    
+
+    public InvalidStatusTransitionException(String message) {
+        super(message);
+    }
+
     public InvalidStatusTransitionException(String currentStatus, String attemptedAction) {
         super(String.format(
-            "Cannot perform '%s' when status is '%s'", 
-            attemptedAction, 
+            "Cannot perform '%s' when status is '%s'",
+            attemptedAction,
             currentStatus
         ));
     }
